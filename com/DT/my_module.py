e8e64ef236b600082a6b3565587748dc4944d013
lee <68258555+Heidiboi@users.noreply.github.com>
2023-05-17 17:55:44 +0900

    Update my_module.py

--- com/DT/my_module.py
+++ com/DT/my_module.py
@@ -1,7 +1,8 @@
 class MyClass:
     def run(self):
-        print("run",__name__)
+        print("run!!",__name__)
 #모듈 작성자 현재 내용을 테스트 하기 위해서
 if __name__=='__main__':
     m=MyClass()
+    
     m.run()
